de6def72c113bd7aeb5a09dd6766b82c11341615
moferaisreal <marcelovprm@gmail.com>
2025-02-26 17:07:19 -0300

    decimal treatment needed

--- Weird Algorithm/script.js
+++ Weird Algorithm/script.js
@@ -1,24 +1,23 @@
 document.getElementById("generateWeirdness").addEventListener("click", (e) => {
   e.preventDefault();
 
-  let weirdNumber = document.getElementById("number");
-  let num = parseInt(weirdNumber.value);
+  let num = document.getElementById("number");
+  let weirdNumber = parseInt(num.value);
   let outputElement = document.getElementById("weirdnessOutput");
-
-  if (isNaN(num) || num <= 0) {
+  if (isNaN(weirdNumber) || weirdNumber <= 0) {
     outputElement.innerHTML = "Please enter a valid positive number!";
     return;
   }
 
-  let outputArray = [num];
+  let outputArray = [weirdNumber];
 
-  while (num !== 1) {
-    if (num % 2 === 0) {
-      num = num / 2;
+  while (weirdNumber !== 1) {
+    if (weirdNumber % 2 === 0) {
+      weirdNumber = weirdNumber / 2;
     } else {
-      num = num * 3 + 1;
+      weirdNumber = weirdNumber * 3 + 1;
     }
-    outputArray.push(num);
+    outputArray.push(weirdNumber);
   }
 
   outputElement.innerHTML = outputArray;
