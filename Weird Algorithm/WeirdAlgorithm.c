85d9d6828e289cb7461e42e19ffa1a83bdb498ac
moferaisreal <marcelovprm@gmail.com>
2025-02-26 17:51:41 -0300

    OUTPUT LIMIT EXCEEDED

--- Weird Algorithm/WeirdAlgorithm.c
+++ Weird Algorithm/WeirdAlgorithm.c
@@ -1,34 +1,34 @@
-#include <stdio.h> 
-
+#include <stdio.h>
+ 
 void generateWeirdness(int num) {
     if (num <= 0) {
         printf("Please enter a valid positive number!\n");
         return;
     }
-
+ 
     printf("%d", num); // Print the first number
-
+ 
     while (num != 1) {
         if (num % 2 == 0) {
             num /= 2;
         } else {
             num = num * 3 + 1;
         }
-        printf(" -> %d", num); // Print each number with an arrow
+        printf(" %d", num); // Print space-separated numbers
     }
-    printf("\n");
+    printf("\n"); // Move to a new line after printing sequence
 }
-
+ 
 int main() {
     int number;
     
-    printf("Enter a positive integer: ");
-    if (scanf("%d", &number) != 1) {
-        printf("Invalid input! Please enter a number.\n");
+    if (scanf("%d", &number) != 1 || number <= 0) { // Ensure valid input
         return 1;
     }
-
+ 
+    printf("\n");  // Ensure a new line before printing output
+ 
     generateWeirdness(number);
-
+ 
     return 0;
 }
