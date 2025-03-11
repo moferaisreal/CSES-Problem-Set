01575fa08ee0c0b90b6850e5577cd158fa6b1e77
moferaisreal <marcelovprm@gmail.com>
2025-03-11 17:30:07 -0300

    finished

--- Weird Algorithm/WeirdAlgorithm.c
+++ Weird Algorithm/WeirdAlgorithm.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
- 
-void generateWeirdness(int num) {
+
+void generateWeirdness(long long num) {
     if (num <= 0) {
         printf("Please enter a valid positive number!\n");
         return;
     }
- 
-    printf("%d", num); // Print the first number
- 
+
+    printf("%lld", num); // Print the first number
+
     while (num != 1) {
         if (num % 2 == 0) {
             num /= 2;
         } else {
             num = num * 3 + 1;
         }
-        printf(" %d", num); // Print space-separated numbers
+        printf(" %lld", num); // Print space-separated numbers
     }
     printf("\n"); // Move to a new line after printing sequence
 }
- 
+
 int main() {
-    int number;
+    long long number;
     
-    if (scanf("%d", &number) != 1 || number <= 0) { // Ensure valid input
+    if (scanf("%lld", &number) != 1 || number <= 0) { // Ensure valid input
         return 1;
     }
- 
-    printf("\n");  // Ensure a new line before printing output
- 
+
     generateWeirdness(number);
- 
+
     return 0;
 }
